be74a7639fc762f512d3d222e42d17cd342d8379
A <gokalp@mail.com>
2021-04-11 15:29:11 +1000

    Convert to a long-running daemon

--- put_data.py
+++ put_data.py
@@ -1,25 +1,59 @@
 #!/usr/bin/env python3
 
-import requests, json, time, psycopg2
+import requests, json, logging, time, psycopg2, signal, sys
 from psycopg2.extensions import AsIs
 
+CHUNK_SIZE = 16
+INTERVAL = 10 # fetch interval in seconds
+LOG_LEVEL = logging.DEBUG
 URL = 'http://inverter/solar_api/v1/GetPowerFlowRealtimeData.fcgi'
 
-resp = requests.get(url=URL).json()
-
-data = {}
-data['time'] = int(time.time())
-data['prod'] = resp['Body']['Data']['Site']['P_PV'] or 0
-data['load'] = round(resp['Body']['Data']['Site']['P_Load']) * -1
-# data['grid'] = round(resp['Body']['Data']['Site']['P_Grid'])
-
-insert_statement = 'insert into electricity (%s) values %s'
-columns = data.keys()
-values = [data[c] for c in columns]
+logging.basicConfig(level=LOG_LEVEL)
 
 db_conn = psycopg2.connect("dbname='home' user='postgres'")
 db_cursor = db_conn.cursor()
-sql = db_cursor.mogrify(insert_statement, (AsIs(','.join(columns)), tuple(values)))
-db_cursor.execute(sql)
-db_conn.commit()
-db_conn.close()
+
+chunk = []
+
+def fetch():
+  resp = requests.get(url=URL).json()
+
+  sample = {}
+  sample['time'] = int(time.time())
+  sample['prod'] = resp['Body']['Data']['Site']['P_PV'] or 0
+  sample['load'] = round(resp['Body']['Data']['Site']['P_Load']) * -1
+# sample['grid'] = round(resp['Body']['Data']['Site']['P_Grid'])
+
+  logging.debug('sample: %s', sample)
+
+  chunk.append(sample)
+
+def insert(chunk):
+  insert_statement = 'insert into electricity (%s) values %s'
+  columns = chunk[0].keys()
+
+  for sample in chunk:
+    values = [sample[c] for c in columns]
+    sql = db_cursor.mogrify(insert_statement, (AsIs(','.join(columns)), tuple(values)))
+    db_cursor.execute(sql)
+
+  db_conn.commit()
+  logging.info('inserted %d samples', len(chunk))
+
+
+def exit_handler():
+  logger.warning('Exiting on signal')
+  db_conn.close()
+  sys.exit(0)
+
+if __name__ == "__main__":
+  signal.signal(signal.SIGINT, exit_handler)
+
+  while True:
+    fetch()
+
+    if len(chunk) == CHUNK_SIZE:
+      insert(chunk)
+      chunk = []
+
+    time.sleep(INTERVAL)
