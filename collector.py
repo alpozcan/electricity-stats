d82a0ed9799e2aade42f4215b8340e588a50fa0c
A <gokalp@mail.com>
2023-01-18 18:42:37 +1100

    Pass through the exception text during fetch(). Lint the script.

--- collector.py
+++ collector.py
@@ -4,9 +4,9 @@ import requests, json, logging, time, psycopg2, signal, sys
 from psycopg2.extras import execute_values
 
 CHUNK_SIZE = 96
-INTERVAL = 10 # fetch interval in seconds
+INTERVAL = 10  # fetch interval in seconds
 LOG_LEVEL = logging.ERROR
-URL = 'http://inverter/solar_api/v1/GetPowerFlowRealtimeData.fcgi'
+URL = "http://inverter/solar_api/v1/GetPowerFlowRealtimeData.fcgi"
 
 logging.basicConfig(level=LOG_LEVEL)
 
@@ -15,43 +15,51 @@ db_cursor = db_conn.cursor()
 
 chunk = []
 
+
 def fetch():
-  resp = requests.get(url=URL).json()
+    resp = requests.get(url=URL).json()
 
-  sample = {}
-  sample['time'] = int(time.time())
-  sample['prod'] = resp['Body']['Data']['Site']['P_PV'] or 0
-  sample['load'] = round(resp['Body']['Data']['Site']['P_Load']) * -1
-# sample['grid'] = round(resp['Body']['Data']['Site']['P_Grid'])
+    sample = {
+        "time": int(time.time()),
+        "prod": resp["Body"]["Data"]["Site"]["P_PV"] or 0,
+        "load": round(resp["Body"]["Data"]["Site"]["P_Load"]) * -1,
+        # "grid": round(resp['Body']['Data']['Site']['P_Grid'])
+    }
+    logging.debug("sample: %s", sample)
+    chunk.append(sample)
 
-  logging.debug('sample: %s', sample)
-
-  chunk.append(sample)
 
 def insert():
-  columns = chunk[0].keys()
-  values_list = [ tuple([sample[c] for c in columns]) for sample in chunk ]
-  execute_values(db_cursor, f'INSERT INTO electricity ({",".join(columns)}) VALUES %s', values_list)
-  db_conn.commit()
-  logging.info('inserted %d samples', len(chunk))
+    columns = chunk[0].keys()
+    values_list = [tuple([sample[c] for c in columns]) for sample in chunk]
+    execute_values(
+        db_cursor,
+        f'INSERT INTO electricity ({",".join(columns)}) VALUES %s',
+        values_list,
+    )
+    db_conn.commit()
+    logging.info("inserted %d samples", len(chunk))
+
 
 def exit_handler(signal_received, frame):
-  logging.warning('Exiting on signal')
-  if len(chunk) > 0: insert()
-  db_conn.close()
-  sys.exit(0)
+    logging.warning("Exiting on signal")
+    if len(chunk) > 0:
+        insert()
+    db_conn.close()
+    sys.exit(0)
+
 
 if __name__ == "__main__":
-  signal.signal(signal.SIGINT, exit_handler)
+    signal.signal(signal.SIGINT, exit_handler)
 
-  while True:
-    try:
-      fetch()
-    except:
-      logging.error('error fetching data')
+    while True:
+        try:
+            fetch()
+        except Exception as e:
+            logging.error(f"{__file__}: Error fetching data\n{e}")
 
-    if len(chunk) == CHUNK_SIZE:
-      insert()
-      chunk = []
+        if len(chunk) == CHUNK_SIZE:
+            insert()
+            chunk = []
 
-    time.sleep(INTERVAL)
+        time.sleep(INTERVAL)
